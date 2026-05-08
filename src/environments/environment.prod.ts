ada0b6bc7f7b8343f45ccde4511b687afd8d1eac
patrimonio-pro <verosilvamona@gmail.com>
2026-05-07 22:20:04 -0300

    config: add production supabase keys for cloud sync

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   supabase: {
     url: 'https://rxsmgtrmjeosmyqdltbn.supabase.co',
     key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ4c21ndHJtamVvc215cWRsdGJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY4OTc4NzAsImV4cCI6MjA5MjQ3Mzg3MH0.YFrzocJNibUj9dqYDktO-burN3axC5Pi1QtAsXdqDyY'
